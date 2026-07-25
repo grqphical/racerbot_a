536e2ad10d9f4b71ed752244b34a755cff185ca2
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-07-25 22:37:30 +0000

    readded formatter, ran formatter

--- src/ftg_node/include/ftg_node.hpp
+++ src/ftg_node/include/ftg_node.hpp
@@ -4,15 +4,16 @@
 #include "sensor_msgs/msg/laser_scan.hpp"
 
 /// @brief The ROS node responsible for controlling the car with Follow The Gap
-class FollowTheGapNode : public rclcpp::Node {
-   public:
-    FollowTheGapNode();
+class FollowTheGapNode : public rclcpp::Node
+{
+public:
+  FollowTheGapNode();
 
-   private:
-    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_msg_publisher;
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscriber;
+private:
+  rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_msg_publisher;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscriber;
 
-    /// @brief Function to be called when the lidar completes a scan
-    /// @param scan_msg The scan data from the lidar
-    void lidar_callback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg);
+  /// @brief Function to be called when the lidar completes a scan
+  /// @param scan_msg The scan data from the lidar
+  void lidar_callback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg);
 };
